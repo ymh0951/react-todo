b36a08b7bc962e0f11dfb3b9278beec118ad6df1
Minho Yoo <ymh0951@naver.com>
2024-03-05 21:21:50 +0900

    App Todo List

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import { useState } from "react"
 
+function fetchTodos() {
+  const result = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const value = localStorage.key(i);
+    result.push(value);
+  }
+
+  return result;
+}
+
 function App() {
-  // const [count, setCount] = useState(0)
-  const [inputText, setInputText] = useState('')
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState(fetchTodos());
 
   const handleInput = (event) => {
     const value = event.target.value;
     setInputText(value)
+  }
+
+  const handleClick = () => {
+    localStorage.setItem(inputText, inputText);
+    setTodos((currentTodos) => {
+      return [...currentTodos, inputText]
+    })
+    setInputText('');
+  }
+  
+  const handleRemove = (todo) => {
+    const result = todos.filter(todoItem => {
+      if (todoItem !== todo) {
+        return true
+      }
+    })
+
+    setTodos(result)
+    localStorage.removeItem(todo);
   }
 
   return (
@@ -14,8 +43,18 @@ function App() {
       <h1>TODO 앱</h1>
       <div>
         <input type="text" value={inputText} onChange={handleInput} />
-        <button>add</button>
+        <button onClick={handleClick}>add</button>
       </div>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              <span>{todo}</span>
+              <button onClick={() => handleRemove(todo)}>remove</button>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
